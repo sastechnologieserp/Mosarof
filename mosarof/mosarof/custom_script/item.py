8c6f89fbb8981afe8377cc73aacce8a70d5298d4
sappu-hub <sarfrasr@gmail.com>
2025-10-15 14:30:23 +0530

    fix: code changed

--- mosarof/mosarof/custom_script/item.py
+++ mosarof/mosarof/custom_script/item.py
@@ -3,5 +3,5 @@ import frappe
 
 def add_custom_barcode_to_child(doc, method):
     """Always barcode to Barcodes child table"""
-    if doc.custom_custom_barcode:
-        doc.append("barcodes", {"barcode": doc.custom_custom_barcode})
+    if doc.custom_barcode:
+        doc.append("barcodes", {"barcode": doc.custom_barcode})
