12037986dfc6f891097cc64e3f8fd17d65aba78a
sappu-hub <sarfrasr@gmail.com>
2025-10-18 13:33:58 +0530

    Feat:Warehouse can be set from opening stock

--- mosarof/hooks.py
+++ mosarof/hooks.py
@@ -28,6 +28,10 @@ app_license = "mit"
 # app_include_css = "/assets/mosarof/css/mosarof.css"
 # app_include_js = "/assets/mosarof/js/mosarof.js"
 
+doctype_js = {
+    "Item Price": "public/js/item_price.js"
+}
+
 # include js, css files in header of web template
 # web_include_css = "/assets/mosarof/css/mosarof.css"
 # web_include_js = "/assets/mosarof/js/mosarof.js"
@@ -146,9 +150,12 @@ app_license = "mit"
 # }
 doc_events = {
     "Item": {
-        "before_save": "mosarof.mosarof.custom_script.item.add_custom_barcode_to_child"
+        "before_save": "mosarof.mosarof.custom_script.item.add_custom_barcode_to_child",
+        "after_insert": "mosarof.mosarof.custom_script.item.create_opening_stock_entry"
     }
 }
+
+
 
 # Scheduled Tasks
 # ---------------
--- mosarof/mosarof/custom_script/item.py
+++ mosarof/mosarof/custom_script/item.py
@@ -3,5 +3,44 @@ import frappe
 
 def add_custom_barcode_to_child(doc, method):
     """Always barcode to Barcodes child table"""
-    if doc.custom_barcode:
-        doc.append("barcodes", {"barcode": doc.custom_barcode})
+    if doc.custom_custom_barcode:
+        doc.append("barcodes", {"barcode": doc.custom_custom_barcode})
+
+def create_opening_stock_entry(doc, method):
+    
+    if not doc.is_stock_item:
+        return
+
+    # Only run if Opening Stock and Warehouse are set
+    if doc.opening_stock and doc.custom_warehouse:
+        opening_qty = doc.opening_stock
+
+        # Get default warehouse from Item Defaults
+        default_warehouse = frappe.db.get_value(
+            "Item Default", {"parent": doc.name}, "default_warehouse"
+        )
+
+        # Check if transfer already exists
+        existing_entry = frappe.db.exists("Stock Entry", {
+            "stock_entry_type": "Material Transfer",
+            "item_code": doc.name
+        })
+        if existing_entry:
+            return
+
+        # Create Material Transfer from default warehouse to selected custom warehouse
+        stock_entry = frappe.new_doc("Stock Entry")
+        stock_entry.stock_entry_type = "Material Transfer"
+        stock_entry.purpose = "Material Transfer"
+
+        stock_entry.append("items", {
+            "item_code": doc.name,
+            "qty": opening_qty,
+            "s_warehouse": default_warehouse,
+            "t_warehouse": doc.custom_warehouse,
+            "basic_rate": doc.valuation_rate or 0
+        })
+
+        stock_entry.insert(ignore_permissions=True)
+        stock_entry.submit()
+
